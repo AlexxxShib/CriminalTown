9b398d08a06bcb6f72a9c661654ca14bd79e073b
ilia isachenko <i.a.isachenko@gmail.com>
2022-03-17 13:27:38 +0700

    added assetville ground tilemap, added prehistoric resource emmitter and force field as well as some Maxim scripts

---
+++ Assets/2_Prototyping/ilia/Scripts/Controllers/AutoCollectable.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Prehistoric.Controllers;
+using UnityEngine;
+
+namespace _0_Project.Scripts.Controllers
+{
+    public class AutoCollectable : MonoBehaviour
+    {
+        private Transform _target;
+        public float Speed = 0.05f;
+
+        private void OnEnable()
+        {
+            _target = FindObjectOfType<CrowdController>().Chief.transform;
+        }
+
+        private void OnDisable()
+        {
+            _target = null;
+        }
+
+        private void Update()
+        {
+            if (_target != null)
+            {
+                transform.position = new Vector3(Mathf.Lerp(transform.position.x, _target.position.x, Speed),
+                    Mathf.Lerp(transform.position.y, _target.position.y, Speed), Mathf.Lerp(transform.position.z, _target.position.z, Speed));
+            }
+        }
+    }
+}
